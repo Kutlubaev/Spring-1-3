a8d0c96be0db09b7e4af315ece8d5400c33ed8f7
Aydar Kutlubaev <Aydark30@gmail.com>
2024-06-13 16:10:45 +0500

    pom update

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Egg6 {
 
-    private final Needle7 needle;
+    private Needle7 needle;
 
-    @Autowired
-    public Egg6(Needle7 needle) {
-        this.needle = needle;
-    }
 
     @Override
     public String toString() {
 
         return ", в яйце иголка " + needle.toString();
+    }
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
     }
 }
